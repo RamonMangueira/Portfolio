19db9226c103ccc6305d1d84c30579a0a3e77ce0
Ramon Mangueira <ramonczsilva@gmail.com>
2021-04-26 17:01:30 -0300

    Configurando módulos
    Dotenv, Body-parser e Mongoose

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 const express = require('express');
-const app = express();
-const api = require('./backend/routes')
+const bodyParser = require('body-parser');
+require('dotenv').config();
 
+const app = express();
+const api = require('./backend/routes');
+
+app.use(bodyParser.json());
+
+//ROTA PRINCIPAL
 app.get('/', (req, res) => {
     res.json({
         'success': true
     });
 });
 
-
+//ROTA DA API
 app.use('/api', api)
 
-const PORT = 3080;
-app.listen(PORT);
+const PORT = process.env.PORT;
+app.listen(PORT, function(){
+    console.log("Servidor rodando na porta:", PORT)
+});
