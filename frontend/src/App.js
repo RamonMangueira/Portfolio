bf7696825e0389e964717c1c130a0fa8e8fe1de8
Ramon Mangueira <61952617+RamonMangueira@users.noreply.github.com>
2021-05-24 21:36:49 -0300

    Set Global Style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,16 @@
 import React from 'react'
+
+
+//Global Style
+import GlobalStyle from './components/GlobalStyle'
+
+// Components
 import AboutUs from './pages/aboutUs'
 
 function App() {
   return (
     <div>
+      <GlobalStyle />
       <AboutUs /> 
     </div>
   );
