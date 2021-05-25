153aa33e9c5eca30e9dafb05434d4adba844d068
Ramon Mangueira <61952617+RamonMangueira@users.noreply.github.com>
2021-05-24 22:00:12 -0300

    Pequenos ajustes no leiaute

--- frontend/src/components/AboutSection.js
+++ frontend/src/components/AboutSection.js
@@ -1,30 +1,28 @@
 import React from 'react'
-import AboutUs from '../pages/aboutUs'
 
-import styled from 'styled-components'
+import {About, Description, Image} from '../styles'
 
 const AboutSection = () => {
     return(
-        <div>
-            <Titulo>
+        <About>
+            <Description>
                 <h2>Ramon Mangueira</h2>
                 <h2>Analista de Sistemas</h2>
-            </Titulo>
 
-            <p>
-                Eu sou Analista de Sistemas, graduado em Análise e Desenvolvimento de Sistemas pela universidade Estácio de Sá. Um apaixonado por inovação e tecnologia.
-            </p>
+                <p>
+                    Eu sou Analista de Sistemas, graduado em Análise e Desenvolvimento de Sistemas pela universidade Estácio de Sá. Um apaixonado por inovação e tecnologia.
+                </p>
 
-            <button>Contacte-me</button>
+                <button>Contacte-me</button>
 
-            <img src='https://sucodemanga.com.br/wp-content/uploads/2018/09/anime-sad-thumb.jpg'></img>
+            </Description>
 
-        </div>
+            <Image>
+                <img src='https://sucodemanga.com.br/wp-content/uploads/2018/09/anime-sad-thumb.jpg'></img>
+            </Image>
+        </About>
     )
 }
 
-const Titulo = styled.div`
-    background: red;
-`;
 
 export default AboutSection
